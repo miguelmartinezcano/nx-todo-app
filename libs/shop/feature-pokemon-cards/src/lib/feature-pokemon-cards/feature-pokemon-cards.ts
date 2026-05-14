6cbd4eeeb663504b9f962678dcec029dec26fdf4
miguelmartinezcano <miguel.martinezcano3@gmail.com>
2026-05-14 15:03:59 -0400

    add want and own buttons

--- libs/shop/feature-pokemon-cards/src/lib/feature-pokemon-cards/feature-pokemon-cards.ts
+++ libs/shop/feature-pokemon-cards/src/lib/feature-pokemon-cards/feature-pokemon-cards.ts
@@ -1,11 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { PokemonSetSkeleton } from '@org/shop/shared-ui';
 import { FeaturePokemonCardsStore } from '../store/feature-pokemon-cards.store';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'lib-feature-pokemon-cards',
@@ -15,7 +17,9 @@ import { FeaturePokemonCardsStore } from '../store/feature-pokemon-cards.store';
     MatChipsModule,
     MatPaginatorModule,
     MatInputModule,
+    MatFormFieldModule,
     PokemonSetSkeleton,
+    MatButtonModule,
   ],
   providers: [FeaturePokemonCardsStore],
   templateUrl: './feature-pokemon-cards.html',
@@ -24,6 +28,47 @@ import { FeaturePokemonCardsStore } from '../store/feature-pokemon-cards.store';
 export class FeaturePokemonCards implements OnInit {
   private readonly route = inject(ActivatedRoute);
   readonly store = inject(FeaturePokemonCardsStore);
+  readonly ownedCards = signal<Set<string>>(new Set());
+  readonly wantedCards = signal<Set<string>>(new Set());
+  readonly quantities = signal<Record<string, number>>({});
+
+  isOwned(cardId: string) {
+    return this.ownedCards().has(cardId);
+  }
+
+  isWanted(cardId: string) {
+    return this.wantedCards().has(cardId);
+  }
+
+  toggleWant(cardId: string) {
+    const next = new Set(this.wantedCards());
+    if (next.has(cardId)) {
+      next.delete(cardId);
+    } else {
+      next.add(cardId);
+    }
+    this.wantedCards.set(next);
+  }
+
+  getQuantity(cardId: string) {
+    return this.quantities()[cardId] ?? 0;
+  }
+
+  onQuantityChange(cardId: string, event: Event) {
+    const value = Number((event.target as HTMLInputElement).value) || 0;
+    this.quantities.update((q) => ({ ...q, [cardId]: value }));
+  }
+
+  toggleOwn(cardId: string) {
+    const next = new Set(this.ownedCards());
+    if (next.has(cardId)) {
+      next.delete(cardId);
+      this.quantities.update((q) => ({ ...q, [cardId]: 0 }));
+    } else {
+      next.add(cardId);
+    }
+    this.ownedCards.set(next);
+  }
 
   ngOnInit() {
     const setId = this.route.snapshot.paramMap.get('setId');
