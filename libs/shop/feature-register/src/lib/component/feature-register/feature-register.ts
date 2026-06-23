3a48975d6b62311bc49b0d747b0c40ee6efde465
miguelmartinezcano <miguel.martinezcano3@gmail.com>
2026-06-23 12:16:20 -0400

    added basic signal form with validation

--- libs/shop/feature-register/src/lib/component/feature-register/feature-register.ts
+++ libs/shop/feature-register/src/lib/component/feature-register/feature-register.ts
@@ -1,10 +1,31 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { RegistrationData } from '../../model/feature-register.model'
+import { form, FormField, required, email, pattern, maxLength } from '@angular/forms/signals';
 
 @Component({
   selector: 'lib-feature-register',
-  imports: [],
+  imports: [FormField],
   templateUrl: './feature-register.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './feature-register.scss',
 })
-export class FeatureRegister {}
+export class FeatureRegister {
+  registrationModel = signal<RegistrationData>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    pin: '',
+  });
+
+  registrationForm = form(this.registrationModel, (schemaPath) => {
+    required(schemaPath.firstName, { message: 'First name is required' });
+    required(schemaPath.lastName, { message: 'Last name is required' });
+    required(schemaPath.email, { message: 'Email is required' });
+    email(schemaPath.email, { message: 'Please enter a valid email address' });
+    required(schemaPath.password, { message: 'Password is required' });
+    required(schemaPath.pin, { message: 'Pin is required' });
+    pattern(schemaPath.pin, /^\d{4}$/, { message: 'Pin must be 4 digits' });
+    maxLength(schemaPath.pin, 4, { message: 'Pin must be 4 digits' });
+  });
+}
